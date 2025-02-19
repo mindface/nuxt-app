2b2b7f1c595d099251f874d0b6e2d2bbd7fb9c23
takanori kamaguchi <in00und@gmail.com>
2025-02-19 10:36:07 +0900

    add api

---
+++ src/composables/useApi.ts
@@ -0,0 +1,18 @@
+function getAutxhHeader() {
+	return {
+		"Content-Type": "application/json",
+		Authorization: `Bearer ${useCookie("auth_token").value}`,
+	};
+}
+
+export function useApi<T>(url: string, async = false, options = {}) {
+	const headers = getAutxhHeader();
+
+	if (async) {
+		return useAsyncData<T>(`api-${url}`, () =>
+			$fetch(url, { headers, ...options }),
+		);
+	} else {
+		return useFetch<T>(url, { headers, ...options });
+	}
+}
